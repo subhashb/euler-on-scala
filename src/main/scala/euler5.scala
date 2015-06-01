a779a3cd905e5f80ed3c8ed00879e5122fe2e9a9
Subhash Bhushan <subhash.bhushan@gmail.com>
2015-06-01 23:33:05 +0530

    Accept console args and invoke Problem execution

---
+++ src/main/scala/euler5.scala
@@ -0,0 +1,5 @@
+object Euler5 {
+  def execute(number: Int = 10) {
+    println(s"Counting upto $number")
+  }
+}
--- src/main/scala/launch.scala
+++ src/main/scala/launch.scala
@@ -1,3 +1,17 @@
 object Launch {
-  def main(args: Array[String]) = println("Hello, World!")
+  def main(args: Array[String]) {
+    if(args.length < 2) {
+      println("Please choose a problem and its input value!")
+      System.exit(0)
+    }
+
+    try {
+      args(0).toInt match {
+        case 5 => Euler5.execute(args(1).toInt)
+        case _ => println("Invalid Problem No.")
+      }
+    } catch {
+      case e:Exception => println("Invalid Problem No.")
+    }
+  }
 }
